7561f87a3a7d5db7400463cc6b072336fc1df87a
Ben Sayeg <bensayeg@gmail.com>
2015-07-17 07:34:11 -0400

    updated MONGO env

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 // config/db.js
+var name = process.env.MONGO_NAME;
+var pw	 = process.env.MONGO_PASSWORD;
     module.exports = {
-        url : 'mongodb://localhost/fathr-dev'
+        url :   "mongodb://" + name + ":" + pw + "@ds047612.mongolab.com:47612/heroku_8z3mdf6z"
     }
